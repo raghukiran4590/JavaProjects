9946851bcc0690c4a330b1721349078e268c01aa
raghukiran4590 <108619893+raghukiran4590@users.noreply.github.com>
2024-10-21 15:37:41 -0400

    Java Projects

---
+++ SampleProject/src/main/java/Package1/ObjectCreation.java
@@ -0,0 +1,23 @@
+package Package1;
+
+public class ObjectCreation implements Cloneable{
+    String name1 = "Object1";
+    String name2 = "Object2";
+    String name3 = "Object3";
+
+    public static void main(String[] args) throws CloneNotSupportedException {
+
+        //Create Object using new keyword
+        ObjectCreation oc1 = new ObjectCreation();
+        System.out.println(oc1.name1);
+
+        //Create object using assignment operator(Deep cloning)
+        ObjectCreation oc2 = oc1;
+        System.out.println(oc1+"    "+oc2);
+
+        //Create Object using Clone method (Shallow Cloning)
+        ObjectCreation oc3 = (ObjectCreation) oc1.clone();
+        System.out.println(oc1+"    "+oc3);
+
+    }
+}
