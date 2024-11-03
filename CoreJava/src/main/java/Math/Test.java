7947710d58d88ab85be5f4965eba75de14b1904d
raghukiran4590 <108619893+raghukiran4590@users.noreply.github.com>
2024-11-02 20:16:44 -0400

    Math Function

---
+++ CoreJava/src/main/java/Math/Test.java
@@ -0,0 +1,22 @@
+package Math;
+
+public class Test {
+    public static void main(String[] args) {
+        Math.max(10,9);
+        Math.min(4,1);
+        Math.abs(-4);
+        System.out.println(Math.E);
+        System.out.println(Math.PI);
+        System.out.println(Math.ceil(3.56));
+        System.out.println(Math.floor(8.1));
+        System.out.println(Math.round(3.44));
+        System.out.println(Math.random());
+        System.out.println(Math.nextAfter(3,4));
+        System.out.println(Math.cos(90));
+        System.out.println(Math.pow(9,2));
+        System.out.println(Math.log(125));
+        System.out.println(Math.log10(100));
+        System.out.println(Math.sin(45));
+        System.out.println(Math.tan(15));
+    }
+}
