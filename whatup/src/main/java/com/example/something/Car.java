5d74c185fc6d63698470bf0a8ed839aa6de1a0a1
raghukiran4590 <raghukiran4590@gmail.com>
2024-12-15 14:13:20 -0500

    Whatup App properties

--- whatup/src/main/java/com/example/something/Car.java
+++ whatup/src/main/java/com/example/something/Car.java
@@ -1,15 +1,20 @@
 package com.example.something;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Car {
 
-public World world;
+    @Autowired
+    private World world;
 
-    @GetMapping("/api/car")
-    public String getHello() {
+    @GetMapping("/api/ok")
+    public String ok() {
+//        world = new World();
         return world.sayHello();
     }
 
