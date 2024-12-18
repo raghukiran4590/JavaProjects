035000809a1dfa3a4687b286e7d0b9bd1b4df8a0
raghukiran4590 <raghukiran4590@gmail.com>
2024-12-18 14:01:10 -0500

    @DBRef

--- journalApp/src/main/java/net/engineeringdigest/journalApp/service/JournalEntryService.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/service/JournalEntryService.java
@@ -6,6 +6,7 @@ import net.engineeringdigest.journalApp.repository.JournalEntryRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -28,12 +29,19 @@ public Optional<JournalEntry> getEntryById(ObjectId myId) {
     return journalEntryRepository.findById(String.valueOf(myId));
 }
 
+@Transactional
 public void saveEntry(JournalEntry journalEntry, String userName) {
-    User user = userService.findByUserName(userName);
-    journalEntry.setDate(LocalDateTime.now());
-    JournalEntry saved = journalEntryRepository.save(journalEntry);
-    user.getJournalEntries().add(saved);
-    userService.saveEntry(user);
+    try {
+        User user = userService.findByUserName(userName);
+        journalEntry.setDate(LocalDateTime.now());
+        JournalEntry saved = journalEntryRepository.save(journalEntry);
+        user.getJournalEntries().add(saved);
+//        user.setUserName(null);
+        userService.saveEntry(user);
+    } catch (Exception e) {
+        System.out.println(e);
+        throw new RuntimeException("An error occurred while executing the program",e);
+    }
 }
 
 public void saveEntry(JournalEntry journalEntry) {
