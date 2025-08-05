0f0906cab114b84270534c9154277220774add50
Kiran, Raghu <raghu.kiran@elevancehealth.com>
2025-08-05 16:44:00 -0400

    LDAP-JWT Authentication

--- LDAP-JWTAuthentication-main/src/main/java/com/ldapjwt/demo/ldap/LDAPConstants.java
+++
@@ -1,9 +0,0 @@
-package com.ldapjwt.demo.ldap;
-
-public interface LDAPConstants{
-
-	String INVALID_TOKEN = "Invalid Token";
-	String VALID_TOKEN = "Valid token for user ";
-	String USERNAME_OR_PASSWORD_INVALID = "Username or Password should not be empty";
-
-}
